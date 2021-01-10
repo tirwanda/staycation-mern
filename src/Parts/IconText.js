f8ce0a72c8e2912dfcdad05fce6f654a451ae58b
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-01-10 20:49:11 +0700

    Create logo

---
+++ src/Parts/IconText.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function IconText() {
+    return (
+        <div>
+            <Button className="brand-text-icon" href="" type="link">
+                Stay<span className="text-gray-900">cation.</span>
+            </Button>
+        </div>
+    )
+}
